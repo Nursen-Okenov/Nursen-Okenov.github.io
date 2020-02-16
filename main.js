9c43f8458c9f10003bf2066716aa116ce0e18957
nursen <nursenokenov57@gmail.com>
2020-02-16 22:30:45 +0600

    added main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll("section");
+let linksInHeader = document.querySelectorAll("a");
+
+for (let index = 0; index < linksInHeader.length; index++) {
+    const element = linksInHeader[index];
+
+    element.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        let ide = element.getAttribute("href");
+        let section = document.querySelector(ide);
+
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+        section.className = "active";
+    });
+}
